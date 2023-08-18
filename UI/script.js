a13ecc2b4f61f6177d8292cbe532818c05dd40ad
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2023-08-18 17:28:20 +0900

    大まかなデザイン完成

--- UI/script.js
+++ UI/script.js
@@ -9,3 +9,19 @@ openButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
+
+
+
+
+/**
+# taskパネルの追加
+@param {string} title
+
+ */
+function add_task(title){
+
+}
+
+function move_to_done(){
+    
+}
