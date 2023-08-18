7d31f09edae859414867f3bb39eef2595bc75751
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2023-08-18 18:07:22 +0900

    taskを追加する関数作成

--- UI/script.js
+++ UI/script.js
@@ -13,15 +13,50 @@ closeButton.addEventListener('click', () => {
 
 
 
+
+
 /**
-# taskパネルの追加
-@param {string} title
-
- */
-function add_task(title){
-
-}
-
-function move_to_done(){
+## taskパネルの追加
+yet領域にtaskパネルを追加します
+@param {String} title
+@param {String} description
+deadlineはnumber型
+@param {Number} deadline
+*/
+function add_task(title, description,deadline){
+    const yet_container = document.getElementById("yet_container");
     
+    //taskパネルを生成
+    const task = document.createElement("div");
+    task.setAttribute("class","task");
+
+    //task title の生成 追加
+    const task_title = document.createElement("h3");
+    task_title.setAttribute("class","task_title");
+    task_title.textContent=title;
+    task.appendChild(task_title);
+
+    //task memo の生成 追加
+    const task_memo = document.createElement("div");
+    task_memo.setAttribute("class","task_memo");
+    task_memo.textContent=description;
+    task.appendChild(task_memo);
+
+    yet_container.appendChild(task);
 }
+
+/**
+## doneにtaskパネルを移動させます
+ */
+function move_to_done(){
+
+}
+
+function main(){
+    add_task(
+        "タイトル","ダミー",
+        10
+    )
+}
+
+main()
