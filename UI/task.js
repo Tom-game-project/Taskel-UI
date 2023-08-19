cc4fa00b64522f64980b81fddb35984594271d67
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2023-08-19 12:45:34 +0900

    サーバーに問い合わせをするための関数を追加

--- UI/task.js
+++ UI/task.js
@@ -1,10 +1,31 @@
 //task event 
 
 /**
+## get_tasks
+### サーバーにtask情報を問い合わせる
+ */
+function get_tasks(){
+
+}
+
+/**
+## get_done_tasks
+### サーバーにtask情報を問い合わせる
+ */
+function get_done_tasks(){
+
+}
+
+
+
+
+
+
+/**
 ##task_done
 ### taskをdone の方に移動させる
 */
-function task_done() {
+function task_done(){
 }
 /**
 ## task_delete
